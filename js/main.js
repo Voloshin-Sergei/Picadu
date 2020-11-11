f91fbfba1568ceeea2d3ada5c10d6d3a550202f1
Voloshin_Sergey <4voloshin@gmail.com>
2020-11-11 11:52:28 +0300

    feat: add user authorization

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
 const passwordInput = document.querySelector('.login-password');
 const loginSignup = document.querySelector('.login-signup');
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
 
 const listUsers = [
   {
@@ -29,30 +31,58 @@ const listUsers = [
 
 const setUsers = {
   user: null,
-  logIn(email, password) {
-  
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('пользователь с такими данными не найден')
+    }
   },
   logOut() {
     console.log('выход')
   },
-  signUp(email, password) {
+  signUp(email, password, handler) {
     if (!this.getUser(email)) {
-      listUsers.push({email, password, displayName: email})
+      const user = {email, password, displayName: email};
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
     } else {
       alert ('пользователь с таким email уже зарегистрирован')
     }
   },
   getUser(email) {
     return listUsers.find(item => item.email === email)
+  },
+  authorizedUser(user) {
+    this.user = user;
+  }
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  console.log('user:', user);
+
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
   }
 };
 
 loginForm.addEventListener('submit', event => {
   event.preventDefault();
-  setUsers.logIn(emailInput.value, passwordInput.value);
+  setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
 });
 
 loginSignup.addEventListener('click', event => {
   event.preventDefault();
-  setUsers.signUp(emailInput.value, passwordInput.value);
+  setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
 });
+
+toggleAuthDom();
