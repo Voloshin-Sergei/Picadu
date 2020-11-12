09939cc7054d8b68c3900ce8078d4eb192cb303d
Voloshin_Sergey <4voloshin@gmail.com>
2020-11-12 11:49:21 +0300

    feat: add user info change function

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ menuToggle.addEventListener("click", function (event) {
   menu.classList.toggle("visible");
 });
 
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 const loginElem = document.querySelector(".login");
 const loginForm = document.querySelector(".login-form");
 const emailInput = document.querySelector(".login-email");
@@ -13,6 +14,12 @@ const passwordInput = document.querySelector(".login-password");
 const loginSignup = document.querySelector(".login-signup");
 const userElem = document.querySelector(".user");
 const userNameElem = document.querySelector(".user-name");
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer =document.querySelector('.edit-container');
+const editUserName = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+const userAvatarElem = document.querySelector('.user-avatar');
 
 const listUsers = [
   {
@@ -32,6 +39,7 @@ const listUsers = [
 const setUsers = {
   user: null,
   logIn(email, password, handler) {
+    if (!regExpValidEmail.test(email)) return alert("email не валиден");
     const user = this.getUser(email);
     if (user && user.password === password) {
       this.authorizedUser(user);
@@ -40,23 +48,34 @@ const setUsers = {
       alert("пользователь с такими данными не найден");
     }
   },
-  logOut() {
-    console.log("выход");
+  logOut(handler) {
+    this.user = null;
+    handler();
   },
   signUp(email, password, handler) {
+    if (!regExpValidEmail.test(email)) return alert("email не валиден");
     if (!email.trim() || !password.trim()) {
       alert('Введите данные')
       return;
     }
 
     if (!this.getUser(email)) {
-      const user = { email, password, displayName: email };
+      const user = { email, password, displayName: email.substring(0, email.indexOf('@'))};
       listUsers.push(user);
       this.authorizedUser(user);
       handler();
     } else {
       alert("пользователь с таким email уже зарегистрирован");
     }
+  },
+  editUser(userName, userPhoto, handler) {
+    if (userName) {
+      this.user.displayName = userName;
+    }
+    if (userPhoto) {
+      this.user.photo = userPhoto;
+    }
+    handler();
   },
   getUser(email) {
     return listUsers.find((item) => item.email === email);
@@ -74,6 +93,7 @@ const toggleAuthDom = () => {
     loginElem.style.display = "none";
     userElem.style.display = "";
     userNameElem.textContent = user.displayName;
+    userAvatarElem.src = user.photo ? user.photo : userAvatarElem.src;
   } else {
     loginElem.style.display = "";
     userElem.style.display = "none";
@@ -90,6 +110,23 @@ loginSignup.addEventListener("click", (event) => {
   event.preventDefault();
   setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
   loginForm.reset();
+});
+
+exitElem.addEventListener('click', event => {
+  event.preventDefault();
+  setUsers.logOut(toggleAuthDom);
+});
+
+editElem.addEventListener('click', event => {
+  event.preventDefault();
+  editContainer.classList.toggle('visible');
+  editUserName.value = setUsers.user.displayName;
+});
+
+editContainer.addEventListener('submit', event => {
+  event.preventDefault();
+  setUsers.editUser(editUserName.value, editPhotoURL.value, toggleAuthDom);
+  editContainer.classList.remove('visible');
 });
 
 toggleAuthDom();
